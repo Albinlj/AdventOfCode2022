21db29609248eda81ef3b28f4882fa89835d8397
Albin Ljunghusen <albinljunghusen@gmail.com>
2022-12-03 08:56:26 +0100

    Day03

--- utilities.ts
+++ utilities.ts
@@ -2,3 +2,14 @@ export const readFile = (fileName: string) => {
 	const path = new URL(".", import.meta.url).pathname + "/days/" + fileName;
 	return Deno.readTextFileSync(path);
 };
+
+export function* chunks<T>(arr: T[], n: number): Generator<T[], void> {
+  for (let i = 0; i < arr.length; i += n) {
+    yield arr.slice(i, i + n);
+  }
+}
+
+export function dump<T>(obj:T) {
+	console.log(obj)
+	return obj
+}
